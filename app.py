b596d17a6fc1ff7dbe24ba5c384f925b4f586899
dangerous barbarians <2557275994@qq.com>
2022-06-30 16:01:34 +0800

    个人主页无参跳转ok

--- app.py
+++ app.py
@@ -7,10 +7,30 @@ app = Flask(__name__, template_folder='templates')
 def index():  # put application's code here
 	return render_template("index.html")
 
-
-@app.route('/detailspage')
-def detailspage():
+# 电影详情界面
+@app.route('/details_page')
+def details_page():
 	return render_template("details_page/千与千寻.html")
+	# return render_template("details_page/教父.html")
+	# return render_template("details_page/爱乐之城.html")
+
+# 个人主页界面
+@app.route('/home_page')
+def home_page():
+	return render_template("home_page.html")
+
+# 用户邀约界面
+# @app.route('/home_page')
+# def home_page():
+# 	return render_template("home_page.html")
+
+# 聊天界面
+# @app.route('/home_page')
+# def home_page():
+# 	return render_template("home_page.html")
+
+
+
 
 
 if __name__ == '__main__':
